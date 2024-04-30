94e8104fdabfd3ee68c5863f52b6dbfe71213e22
Taz-war <fahimtazwer@gmail.com>
2024-05-01 02:38:58 +0600

    aha

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image";
 import NewHome from '@/components/Home'
 import Example from "@/components/Example";
@@ -5,6 +6,7 @@ import MultiStepper from "@/components/MultiStep";
 import Step2 from "@/components/steps/Step2";
 import HomeContainer from "@/components/Home";
 import CustomSelect from "@/components/Example";
+import CustomAutoComplete from "@/components/atoms/CustomAutoComplete";
 const options = [
   { id: 1, label: "Apple" },
   { id: 2, label: "Samsung" },
@@ -18,7 +20,8 @@ export default function Home() {
       {/* <CustomSelect options={options} placeholder="Device brand" /> */}
       {/* <MultiStepper />
       <Step2 /> */}
-      <HomeContainer/>
+      <CustomAutoComplete />
+      {/* <HomeContainer/> */}
     </main>
   );
 }
