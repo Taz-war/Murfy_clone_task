4fcf2be259fe87ec25406ef263ca4622a9341e73
Taz-war <fahimtazwer@gmail.com>
2024-05-06 02:36:51 +0600

    responsive

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client"
+import React, { useState, useEffect } from "react";
 import Button from "./atoms/Button";
 import MultiStepper from "./MultiStep";
 import Step2 from "./steps/step2/Step2";
@@ -8,20 +9,39 @@ import Step3 from "./steps/step3/Step3";
 import Step4 from "./steps/step4/Step4";
 import Stepper from "./Example";
 
-const HomeContainer = () => {
+const HomeContainer: React.FC = () => {
+  const [showStepper, setShowStepper] = useState<boolean>(true);
+
+  // Custom function to update the visibility of the Stepper
+  const handleResize = () => {
+    const shouldShow = window.innerWidth >= 950; // Custom screen width (1024px as an example)
+    setShowStepper(shouldShow);
+  };
+
+  useEffect(() => {
+    handleResize(); // Set initial state
+    window.addEventListener("resize", handleResize); // Add resize listener
+    return () => window.removeEventListener("resize", handleResize); // Cleanup listener
+  }, []);
   return (
-    <div className=" grid grid-cols-12 gap-4">
-      <div className=" col-span-8 p-3">
+    <div className=" grid grid-cols-12 gap-4 p-3">
+      {/* <div className="col-span-12 md:col-span-8"> */}
+      <div className={`col-span-${showStepper ? "8" : "12"}`}>
         {/* <Step2 /> */}
-        <SubStep1 />
-        <SubStep2 />
-        <Step3></Step3>
+        {/* <SubStep1 /> */}
+        {/* <SubStep2 /> */}
+        {/* <Step3></Step3> */}
         <Step4 />
       </div>
-      <div className=" col-span-4">
-        {/* <MultiStepper /> */}
+      {showStepper && (
+        <div className="col-span-4">
+          <Stepper />
+        </div>
+      )}
+      {/* <div className=" hidden md:col-span-4 md:block">
+        <MultiStepper />
         <Stepper />
-      </div>
+      </div> */}
     </div>
   );
 };
--- components/atoms/CustomAutoComplete.tsx
+++ components/atoms/CustomAutoComplete.tsx
@@ -1,85 +1,103 @@
 // Correct import statements
-"use client"
-import { Fragment, useState, useRef, useEffect } from 'react';
-import { Combobox, Transition } from '@headlessui/react';
-import { CheckIcon, ChevronUpDownIcon, MapPinIcon } from '@heroicons/react/20/solid';
+"use client";
+import { Fragment, useState, useRef, useEffect } from "react";
+import { Combobox, Transition } from "@headlessui/react";
+import {
+    CheckIcon,
+    ChevronUpDownIcon,
+    MapPinIcon,
+} from "@heroicons/react/20/solid";
 import "@/components/atoms/customCss.css";
 
 interface Person {
-  id: number;
-  name: string;
+    id: number;
+    name: string;
 }
 
 const people: Person[] = [
-    { id: 1, name: 'Wade Cooper' },
-    { id: 2, name: 'Arlene Mccoy' },
-    { id: 3, name: 'Devon Webb' },
-    { id: 4, name: 'Tom Cook' },
-    { id: 5, name: 'Tanya Fox' },
-    { id: 6, name: 'Hellen Schmidt' },
+    { id: 1, name: "Wade Cooper" },
+    { id: 2, name: "Arlene Mccoy" },
+    { id: 3, name: "Devon Webb" },
+    { id: 4, name: "Tom Cook" },
+    { id: 5, name: "Tanya Fox" },
+    { id: 6, name: "Hellen Schmidt" },
 ];
 
 interface CustomAutoCompleteProps {
-  borderRadius?: string;
-  label?: string;
-  type?: "location" | "default";
+    borderRadius?: string;
+    label?: string;
+    type?: "location" | "default";
 }
 
 export default function CustomAutoComplete(props: CustomAutoCompleteProps) {
     console.log(props.borderRadius); // Debugging output
     const [selected, setSelected] = useState<Person | null>(null);
-    const [query, setQuery] = useState<string>('');
+    const [query, setQuery] = useState<string>("");
     const [isFocused, setIsFocused] = useState<boolean>(false);
     const inputRef = useRef<HTMLInputElement | null>(null);
 
     const filteredPeople: Person[] =
-        query === ''
+        query === ""
             ? people
             : people.filter((person) =>
-                person.name.toLowerCase().replace(/\s+/g, '').includes(query.toLowerCase().replace(/\s+/g, ''))
+                person.name
+                    .toLowerCase()
+                    .replace(/\s+/g, "")
+                    .includes(query.toLowerCase().replace(/\s+/g, ""))
             );
 
-  useEffect(() => {
-    if (!query && !isFocused && inputRef.current) {
-      setSelected(null);
-    }
-  }, [query, isFocused]);
+    useEffect(() => {
+        if (!query && !isFocused && inputRef.current) {
+            setSelected(null);
+        }
+    }, [query, isFocused]);
 
-  const handleFocus = () => {
-    setIsFocused(true);
-  };
+    const handleFocus = () => {
+        setIsFocused(true);
+    };
 
-  const handleBlur = () => {
-    setIsFocused(false);
-  };
+    const handleBlur = () => {
+        setIsFocused(false);
+    };
 
     return (
         <div className="w-full">
             <Combobox value={selected} onChange={setSelected}>
-                <div className="relative mt-1">
-                    <div className={`relative w-full cursor-default overflow-hidden text-left border border-gray-300 shadow-md focus-within:border-gray-300 focus-within:ring-1 focus-within:ring-gray-300 ${props.borderRadius === '50%' ? "rounded-full" : "rounded-lg"}`}>
+                <div className="relative float-label-input m-0">
+                    <div
+                        className={`relative w-full cursor-default overflow-hidden text-left border border-gray-300 focus-within:border-gray-300 focus-within:ring-1 focus-within:ring-gray-300 ${props.borderRadius === "50%" ? "rounded-full" : "rounded-lg"
+                            }`}
+                    >
                         <Combobox.Input
-                            className="w-full py-3 pl-3 pr-10 text-sm leading-5 h-166 text-gray-500 focus:outline-none"
-                            displayValue={(person: Person) => person?.name || ''}
+                            className="w-full py-3 pl-3 pr-10 text-sm leading-normal h-16 text-gray-800 font-medium focus:outline-none"
+                            displayValue={(person: Person) => person?.name || ""}
                             onChange={(event) => setQuery(event.target.value)}
                             onFocus={handleFocus}
                             onBlur={handleBlur}
                             placeholder={!isFocused && !query ? props.label : ""}
                             ref={inputRef}
                         />
-                        <label htmlFor="combobox-input" className={`absolute left-3 transition-all duration-200 ease-in-out text-sm pointer-events-none ${isFocused || query ? 'top-0 text-gray-400 text-xs' : 'top-1/2 -translate-y-1/2 text-gray-400'}`}>
+                        <label
+                            htmlFor="combobox-input"
+                            className={`absolute left-3 transition-all duration-100 ease-in text-sm pointer-events-none ${isFocused || query
+                                    ? "top-[20%] text-gray-400"
+                                    : "top-1/2 -translate-y-1/2 text-gray-400"
+                                }`}
+                        >
                             {props.label}
                         </label>
                         <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-                            {props.type === "location" ?
+                            {props.type === "location" ? (
                                 <MapPinIcon
                                     className="h-5 w-5 text-gray-500"
                                     aria-hidden="true"
                                 />
-                                : <ChevronUpDownIcon
+                            ) : (
+                                <ChevronUpDownIcon
                                     className="h-5 w-5 text-gray-500"
                                     aria-hidden="true"
-                                />}
+                                />
+                            )}
                         </Combobox.Button>
                     </div>
                     <Transition
@@ -87,10 +105,10 @@ export default function CustomAutoComplete(props: CustomAutoCompleteProps) {
                         leave="transition ease-in duration-100"
                         leaveFrom="opacity-100"
                         leaveTo="opacity-0"
-                        afterLeave={() => setQuery('')}
+                        afterLeave={() => setQuery("")}
                     >
-                        <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                            {filteredPeople.length === 0 && query !== '' ? (
+                        <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                            {filteredPeople.length === 0 && query !== "" ? (
                                 <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
                                     Nothing found.
                                 </div>
@@ -99,17 +117,23 @@ export default function CustomAutoComplete(props: CustomAutoCompleteProps) {
                                     <Combobox.Option
                                         key={person.id}
                                         className={({ active }) =>
-                                            `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-gray-200 text-black' : 'text-gray-900'}`
+                                            `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? "bg-gray-200 text-black" : "text-gray-900"
+                                            }`
                                         }
                                         value={person}
                                     >
                                         {({ selected, active }) => (
                                             <>
-                                                <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
+                                                <span
+                                                    className={`block truncate ${selected ? "font-medium" : "font-normal"
+                                                        }`}
+                                                >
                                                     {person.name}
                                                 </span>
                                                 {selected ? (
-                                                    <span className={`absolute inset-y-0 left-0 flex items-center pl-3 text-black`}>
+                                                    <span
+                                                        className={`absolute inset-y-0 left-0 flex items-center pl-3 text-black`}
+                                                    >
                                                         <CheckIcon className="h-5 w-5" aria-hidden="true" />
                                                     </span>
                                                 ) : null}
